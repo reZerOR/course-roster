b9faefedcc611c80dc6598db4c3d88885b735c6b
reZerOR <gkraihan14k@gmail.com>
2023-09-15 12:19:33 +0600

    problem solved file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import Header from "./components/header/Header";
-import Blogs from "./components/blogs/Blogs";
-import Bookmarks from "./components/bookmarks/Bookmarks";
-import { useState } from "react";
+import "./App.css";
+
 function App() {
-  const [bookmarks, setBookmarks] = useState([]);
-  const [readtime, setReadtime] = useState(0);
-
-  const handleAddTime = (time) => {
-    const newTime = readtime + time;
-    setReadtime(newTime);
-  };
-
-  const handleAddToBookmarks = (blog) => {
-    const newBookMarks = [...bookmarks, blog];
-    setBookmarks(newBookMarks);
-  };
   return (
     <>
-      <Header></Header>
-      <div className="md:flex max-w-7xl mx-auto mt-6">
-        <Blogs
-          handleAddToBookmarks={handleAddToBookmarks}
-          handleAddTime={handleAddTime}
-        ></Blogs>
-        <Bookmarks time={readtime} bookmarks={bookmarks}></Bookmarks>
-      </div>
+      <h1 className="text-4xl font-bold">Vite + React</h1>
     </>
   );
 }
